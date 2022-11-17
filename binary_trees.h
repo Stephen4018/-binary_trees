cad72911008cf227b82c47da9c4f82bd34e5b146
egjallo <egjallo@insistglobal.com>
2022-11-17 16:27:09 +0000

    create node

--- binary_trees.h
+++ binary_trees.h
@@ -1,11 +1,10 @@
-#ifndef BINARY_TREES_H_
+#ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
 #include <stddef.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -15,18 +14,29 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
-
-typedef struct binary_tree_s binary_tree_t;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
+/**
+ * struct binary_list - Binary tree list
+ *
+ * @n: pointer to node
+ * @next: next node
+ */
+typedef struct binary_list
+{
+	const bst_t *n;
+	struct binary_list *next;
+} queue;
+
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 #endif /* _BINARY_TREES_H_ */
